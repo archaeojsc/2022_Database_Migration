093e99908a506851311b49126aa772be532db254
J. Scott Cardinal <archaeo_scott@yahoo.com>
2022-11-16 15:50:03 -0500

    refactor, experiment with data schema retrieval

--- db_connection_tests.py
+++ db_connection_tests.py
@@ -4,41 +4,66 @@ import pyodbc
 import pandas as pd
 import os
 
-# %% Connection info
+# %% Get file paths for all database files in directories
+
+db_files_list = []
+
+file_directory_start = [
+    "X:\\CRSP Databases",
+    "X:\\CRSP Fieldwork 2020",
+    "X:\\CRSP Fieldwork 2021",
+    "X:\\CRSP Fieldwork 2022",
+]
+
+# start_dir = os.getcwd()
+
+# db_file_suffix = (".accdb", ".mdb.old", ".mdb", ".DBF") # Include old files
+
+db_file_suffix = (".accdb", ".mdb")  # Only include active files
+
+for top_directory in file_directory_start:
+    for root, dirs, files in os.walk(top_directory):
+        for file in files:
+            if file.endswith(db_file_suffix):
+                db_files_list.append(os.path.join(root, file))
+
+
+# %% ODBC Connection info
 
 db_driver = "{Microsoft Access Driver (*.mdb, *.accdb)}"
 
-# Path for office workstation
+# Testing path for office workstation
 db_path = "C:\\Users\\scardina\\Documents\\Projects\\Active Projects\\2022_Database_Migration\\1BOW.00.101 Prattsville (10-2014).accdb"
 
-# Path for home workstation
+# Testing path for home workstation
 # db_path = "C:\\Users\\Scott\\Documents\\2022_Database_Migration\\1BOW.00.101 Prattsville (10-2014).accdb"
 
+# ODBC connection string
 conn_str = rf"DRIVER={db_driver};" rf"DBQ={db_path};"
 
-# %% Open connection and cursor
+# %% Open ODBC connection and cursor
 
 conn = pyodbc.connect(conn_str)
 cur = conn.cursor()
 
-
-# %% Print tables
+# %% Retrieve list of database tables, excluding system tables and query views
 
 my_tables = [t.table_name for t in cur.tables(tableType="TABLE")]
+
 
 # %% Table columns
 
 for t in my_tables:
     print("Table:\t", t, "\n")
     for c in cur.columns(table=t):
-        print("\t", c.column_name, "(", c.type_name, ")")
+        print("\t", c.column_name, c.type_name, c.sql_data_type, c.is_nullable)
     print("\n")
 
 # %% Primary Keys
 
 for t in my_tables:
     print("Table:\t", t, "\n")
-    for p_key in cur.foreignKeys(table=t):
+    for p_key in cur.primaryKeys(table=t):
         print("\t", p_key)
     print("\n")
 
@@ -62,29 +87,6 @@ for t in my_tables:
 # %% Close connection
 
 conn.close()
-
-
-# %% Get file paths for database files in directory
-
-db_files_list = []
-
-file_directory_start = [
-    "X:\\CRSP Databases",
-    "X:\\CRSP Fieldwork 2020",
-    "X:\\CRSP Fieldwork 2021",
-    "X:\\CRSP Fieldwork 2022",
-]
-
-# start_dir = os.getcwd()
-
-# db_file_suffix = (".accdb", ".mdb.old", ".mdb", ".DBF")
-db_file_suffix = (".accdb", ".mdb")
-
-for top_directory in file_directory_start:
-    for root, dirs, files in os.walk(top_directory):
-        for file in files:
-            if file.endswith(db_file_suffix):
-                db_files_list.append(os.path.join(root, file))
 
 
 # %%
